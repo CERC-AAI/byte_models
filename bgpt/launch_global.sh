00f187a6e256a6f3c8c7eee20d0c899d90be66ba
George Adams <george.adamopoulos13@gmail.com>
2024-04-07 14:06:03 -0400

    fixing reload & config paths

--- bgpt/launch_global.sh
+++ bgpt/launch_global.sh
@@ -9,6 +9,7 @@
 
 SRC_DIR="/lustre/orion/csc590/scratch/george-adams/bgpt2/byte_models/bgpt"
 CONFIG_PATH="/lustre/orion/csc590/scratch/george-adams/bgpt2/byte_models/bgpt/configs/config_110M.yaml"
+RELOAD_CONFIG_PATH="/lustre/orion/csc590/scratch/george-adams/bgpt2/byte_models/bgpt/configs/reload_config_110M.yaml"
 CONDA_ENV_PATH="/lustre/orion/csc590/scratch/george-adams/conda_envs/bgpt"
 MODULE_LOAD_VERSION="rocm/5.2"
 
@@ -25,9 +26,9 @@ do
 
   # Submit job and capture the job ID
   if [ "$DEPENDENCY" == "none" ]; then
-    JOB_SUBMIT_OUTPUT=$(sbatch --job-name=$JOB_NAME --output=${SRC_DIR}/logs/%x-%j.out --time=2:00:00 --partition=batch --nodes=32 launch.sh)
+    JOB_SUBMIT_OUTPUT=$(sbatch --job-name=$JOB_NAME --output=${SRC_DIR}/logs/%x-%j.out --time=2:00:00 --partition=batch --nodes=32 launch.sh --train-config-path ${CONFIG_PATH})
   else
-    JOB_SUBMIT_OUTPUT=$(sbatch --dependency=afterany:$DEPENDENCY --job-name=$JOB_NAME --output=${SRC_DIR}/logs/%x-%j.out --time=2:00:00 --partition=batch --nodes=32 conditional_launch.sh)
+    JOB_SUBMIT_OUTPUT=$(sbatch --dependency=afterany:$DEPENDENCY --job-name=$JOB_NAME --output=${SRC_DIR}/logs/%x-%j.out --time=2:00:00 --partition=batch --nodes=32 conditional_launch.sh --train-config-path ${RELOAD_CONFIG_PATH})
   fi
 
   # Extract job ID from the submission output
